18a178f27fe8d9b3a536f2bf42a6a94f6346b7f4
muminovabdulloh <muminovabdullokh@gmail.com>
2024-06-08 19:36:31 +0500

    Abdulloh Muminov, Colors and Route names compleated

---
+++ lib/core/style/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+@immutable
+final class AppColors{
+  const AppColors._();
+
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+  static const Color transparent = Color(0x00ffffff);
+  static const Color l00B533 = Color(0xff00B533);
+  static const Color l121212 = Color(0xff121212);
+  static const Color lC1C1C1 = Color(0xffC1C1C1);
+  static const Color lD9D9D9 = Color(0xffD9D9D9);
+  static const Color l264653 = Color(0xff264653);
+  static const Color lF5F5F5 = Color(0xffF5F5F5);
+  static const Color lFF0000 = Color(0xffFF0000);
+  static const Color white30 = Colors.white30;
+  static const Color white50 = Colors.white54;
+}
