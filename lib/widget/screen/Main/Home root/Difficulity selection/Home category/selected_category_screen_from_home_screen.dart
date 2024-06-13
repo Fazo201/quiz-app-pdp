c55ebab9ef9f8edab8813a31e781adc778d7745f
PdpQuiz <pdp.quiz.application@gmail.com>
2024-06-13 21:01:42 +0500

    dropdown to'g'irlandi

--- lib/widget/screen/Main/Home root/Difficulity selection/Home category/selected_category_screen_from_home_screen.dart
+++ lib/widget/screen/Main/Home root/Difficulity selection/Home category/selected_category_screen_from_home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quiz_app/core/route/app_route_path.dart';
-
 import 'package:quiz_app/core/style/app_colors.dart';
 import 'package:quiz_app/core/style/app_images.dart';
 import 'package:quiz_app/core/style/app_text_style.dart';
@@ -18,7 +17,7 @@ class _SelectedCategoryScreenFromHomeScreenState
     extends State<SelectedCategoryScreenFromHomeScreen> {
   int current = 0;
   String? value;
-  bool isOpen = false;
+  late List<bool> _isOpen;
 
   final List<String> modules = [
     "1-modul",
@@ -46,19 +45,24 @@ class _SelectedCategoryScreenFromHomeScreenState
   ];
 
   @override
+  void initState() {
+    super.initState();
+    _isOpen = List<bool>.filled(dropDownMenuList.length, false);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
       appBar: AppBar(
+        surfaceTintColor: AppColors.white,
         backgroundColor: AppColors.white,
         toolbarHeight: 180,
         automaticallyImplyLeading: false,
-        elevation: 0,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Column(
-              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Row(
                   children: [
@@ -95,6 +99,155 @@ class _SelectedCategoryScreenFromHomeScreenState
           ],
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: SizedBox(
+                  height: 38,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: modules.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            current = index;
+                          });
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 5),
+                          width: 74,
+                          decoration: BoxDecoration(
+                            color: current == index
+                                ? AppColors.l00B533
+                                : AppColors.lF5F5F5,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              modules[index],
+                              style: const AppTextStyle().richText?.copyWith(
+                                    fontSize: 12,
+                                    color: current == index
+                                        ? AppColors.white
+                                        : AppColors.black,
+                                  ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: dropDownMenuList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: AppColors.lF5F5F5,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Text(
+                            '${index + 1}.',
+                            style: const AppTextStyle().titleSmall?.copyWith(
+                              fontSize: 16
+                            ),
+                          ),
+                          title: Text(
+                            dropDownMenuList[index],
+                            style: const AppTextStyle().titleSmall?.copyWith(
+                              fontSize: 16
+                            ),
+                          ),
+                          trailing: _isOpen[index]
+                                ? const Icon(Icons.close,color: Colors.red)
+                                : const Icon(Icons.add),
+                          onTap: (){
+                            setState(() {
+                              _isOpen[index] = !_isOpen[index];
+                            });
+                          },
+                        ),
+                        if (_isOpen[index])
+                          Column(
+                            children: [
+                              ListTile(
+                                title: Text(dropDownMenuList[0]),
+                              ),
+                              ListTile(title: Text(dropDownMenuList[1])),
+                              ListTile(
+                                title: Text(dropDownMenuList[2]),
+                              ),
+                              ListTile(
+                                title: Text(dropDownMenuList[3]),
+                              ),
+                              ListTile(
+                                title: Text(dropDownMenuList[4]),
+                              ),
+                              ListTile(
+                                title: Text(dropDownMenuList[5]),
+                              ),
+                              ListTile(
+                                title: Text(dropDownMenuList[6]),
+                              ),
+                              ListTile(
+                                title: Text(dropDownMenuList[7]),
+                              ),
+                              ListTile(
+                                title: Text(dropDownMenuList[8]),
+                              ),
+                              ListTile(
+                                title: Text(dropDownMenuList[9]),
+                              ),
+                              ListTile(
+                                trailing: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: MaterialButton(
+                                    color: AppColors.l00B533,
+                                    onPressed: () {
+                                      context.go(
+                                          "${AppRoutePath.home}/${AppRoutePath.difficultySelection}/${AppRoutePath.homeCategory}/${AppRoutePath.quizGame}");
+                                    },
+                                    child: const Text(
+                                      "Start Quiz",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem<String>(
+        value: item,
+        child: Text(
+          item,
+          style: const AppTextStyle().titleSmall?.copyWith(fontSize: 16),
+        ),
+      );
+}
+
+
       // body: Padding(
       //   padding: const EdgeInsets.symmetric(horizontal: 20),
       //   child: Column(
@@ -169,101 +322,3 @@ class _SelectedCategoryScreenFromHomeScreenState
       //     ],
       //   ),
       // ),
-      body: Expanded(
-        child: ListView.builder(
-          itemCount: modules.length,
-          itemBuilder: (context, index) {
-            return Card(
-              color: AppColors.lF5F5F5,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Text(
-                      '${index + 1}.',
-                      // style: AppTextStyle.bodyLarge,
-                    ),
-                    title: Text(
-                      modules[index],
-                      // style: AppTextStyle.bodyLarge11,
-                    ),
-                    trailing: InkWell(
-                      radius: 20,
-                      child: isOpen
-                          ? const Icon(
-                              Icons.close,
-                              color: Colors.red,
-                            )
-                          : const Icon(Icons.add),
-                      onTap: () {
-                        setState(() {
-                          isOpen = !isOpen;
-                        });
-                      },
-                    ),
-                  ),
-                  if(isOpen)
-                  Column(children: [
-                    ListTile(
-                      title: Text(dropDownMenuList[0]),
-                    ),
-                    ListTile(title: Text(dropDownMenuList[1])),
-                    ListTile(
-                      title: Text(dropDownMenuList[2]),
-                    ),
-                    ListTile(
-                      title: Text(dropDownMenuList[3]),
-                    ),
-                    ListTile(
-                      title: Text(dropDownMenuList[4]),
-                    ),
-                    ListTile(
-                      title: Text(dropDownMenuList[5]),
-                    ),
-                    ListTile(
-                      title: Text(dropDownMenuList[6]),
-                    ),
-                    ListTile(
-                      title: Text(dropDownMenuList[7]),
-                    ),
-                    ListTile(
-                      title: Text(dropDownMenuList[8]),
-                    ),
-                    ListTile(
-                      title: Text(dropDownMenuList[9]),
-                    ),
-                    ListTile(
-                      trailing: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: MaterialButton(
-                          color: AppColors.l00B533,
-                          onPressed: () {
-                            context.go("${AppRoutePath.home}/${AppRoutePath.difficultySelection}/${AppRoutePath.homeCategory}/${AppRoutePath.quizGame}");
-                          },
-                          child: const Text(
-                            "Start Quiz",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
-
-  DropdownMenuItem<String> buildMenuItem(String item) =>
-    DropdownMenuItem<String>(
-      value: item,
-      child: Text(
-        item,
-        style: const AppTextStyle().titleSmall?.copyWith(fontSize: 16),
-      ),
-    );
-}
