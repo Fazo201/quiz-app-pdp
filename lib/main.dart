3962a755611a3944a6867f6193cbf352b1b75e41
Fazo201 <erkaboyevfazogir98@gmail.com>
2024-06-18 21:07:18 +0500

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'dart:async';
+// import 'dart:async';
 
-import 'package:flutter/material.dart';
-import 'package:quiz_app/app.dart';
-import 'package:quiz_app/setup.dart';
+// import 'package:flutter/material.dart';
+// import 'package:quiz_app/app.dart';
+// import 'package:quiz_app/setup.dart';
 
-Future<void> main() async {
-  await setup();
-  runApp(const App());
-}
+// Future<void> main() async {
+//   await setup();
+//   runApp(const App());
+// }
